116fab0e252571930dd0b8e466ce8b5fd92253fa
mdashmiller <m.robert.miller@gmail.com>
2018-12-17 15:06:08 -0600

    added materialize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Messages Go Here!!!</h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+        	<div className="App">
+      			<div className="container">
+        			<h1 className="center-align blue-text text-darken-3">Messages Go Here!!!</h1>
+      			</div>
+        	</div>
+    	)
+  	}
 }
 
 export default App
