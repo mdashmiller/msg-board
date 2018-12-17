cb604d734c716507c6fd8de4acbd5de70d2c6c37
mdashmiller <m.robert.miller@gmail.com>
2018-12-17 16:07:59 -0600

    navbar structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
 				  	<div className="App">
-							<div className="container">
-				  				<h1 className="center-align blue-text text-darken-3">Messages Go Here!!!</h1>
-							</div>
+							<Navbar />
 				  	</div>
 			</BrowserRouter>
     	)
