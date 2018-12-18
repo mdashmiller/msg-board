9056e7606c80cb6635e0f69786b72d2c6224c70e
mdashmiller <m.robert.miller@gmail.com>
2018-12-18 15:06:07 -0600

    basic layout

---
+++ src/components/posts/CreatePost.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class CreatePost extends Component {
+
+	state = {
+		title: '',
+		message: ''
+	}
+
+	// component methods
+
+	handleChange = e =>
+		this.setState({
+			[e.target.id]: e.target.value
+		})
+
+	handleSubmit = e => {
+		e.preventDefault()
+		console.log(this.state)
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<form onSubmit={this.handleSubmit} className="white">
+					<h5 className="grey-text text-darken-3">Create Post</h5>
+					<div className="input-field">
+						<label htmlFor="title">Title</label>
+						<input type="text" id="title" onChange={this.handleChange} />
+					</div>
+					<div className="input-field">
+						<label htmlFor="message">Message</label>
+						<textarea id="message" className="materialize-textarea" onChange={this.handleChange}></textarea>
+					</div>
+					<div className="input-field">
+						<button className="btn purple lighten-1 z-depth-0">post</button>
+					</div>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default CreatePost
---
+++ src/components/posts/PostSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PostSummary = () => {
+	return (
+		<div className="card z-depth-0 post-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">Post Title</span>
+				<p>Posted by Robo Robo</p>
+				<p className="grey-text">7th October, 11am</p>
+			</div>
+		</div>
+	)
+}
+
+export default PostSummary
