c94c04f005f5fe94c2e0371aa88a7528515c9b17
mdashmiller <m.robert.miller@gmail.com>
2018-12-19 16:40:42 -0600

    synced post details to db

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,22 +1,47 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const PostDetails = props => {
-	const id = props.match.params.id
+	const { post } = props
 
-	return (
-		<div className="container section post-details">
-			<div className="card z-depth-0">
-				<div className="card-content">
-					<span className="card-title">Post Title - {id}</span>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga odit eaque, corporis vero tempore.</p>
-				</div>
-				<div className="card-action grey lighten-4 grey-text">
-					<div>Posted by Robo Robo</div>
-					<div>7th October, 11am</div>
+	if (post) {
+		return (
+			<div className="container section post-details">
+				<div className="card z-depth-0">
+					<div className="card-content">
+						<span className="card-title">{post.title}</span>
+						<p>{post.message}</p>
+					</div>
+					<div className="card-action grey lighten-4 grey-text">
+						<div>Posted by {post.authorFirstName} {post.authorLastName}</div>
+						<div>7th October, 11am</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	} else {
+		return (
+			<div className="container center">
+				<p>Loading post...</p>
+			</div>
+		)
+	}
 }
 
-export default PostDetails
+const mapStateToProps = (state, ownProps) => {
+	const id = ownProps.match.params.id
+	const posts = state.firestore.data.posts
+	const post = posts ? posts[id] : null
+	return {
+		post
+	}
+}
+
+export default compose(
+	connect(mapStateToProps),
+	firestoreConnect([
+		{ collection: 'posts' }
+	])
+)(PostDetails)
--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PostSummary from './PostSummary'
+import { Link } from 'react-router-dom'
 
 const PostList = ({ posts }) =>
 	<div className="post-list section">
 		{
 			posts && posts.map(post =>
-				<PostSummary post={post} key={post.id} />
+				<Link to={'/post/' + post.id} key={post.id}>
+					<PostSummary post={post} />
+				</Link>
 			)
 		}
 	</div>
