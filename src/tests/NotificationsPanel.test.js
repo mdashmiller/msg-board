8faa2f29afc5c73e916fc2055b7b61ee74871822
mdashmiller <m.robert.miller@gmail.com>
2019-01-28 18:22:36 -0600

    unit test setup with enzyme

---
+++ src/tests/NotificationsPanel.test.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import renderer from 'react-test-renderer'
+// // import Enzyme, { shallow } from 'enzyme'
+// // import Adapter from 'enzyme-adapter-react-16'
+
+// import NotificationsPanel from '../components/dashboard/NotificationsPanel'
+
+// // Enzyme.configure({ adapter: new Adapter() })
+
+// describe('<NotificationsPanel /> rendering', () => {
+
+// 	it('renders without crashing', () => {
+// 	    const div = document.createElement('div')
+// 	    ReactDOM.render(<NotificationsPanel />, div)
+// 	    ReactDOM.unmountComponentAtNode(div)
+// 	})
+
+// 	test('has a valid snapshot', () => {
+// 		const component = renderer.create(<NotificationsPanel />)
+// 		let tree = component.toJSON()
+// 		expect(tree).toMatchSnapshot()
+// 	})
+// })
