193a1e2eec4634506aa283f54def7bdf0e109b50
mdashmiller <m.robert.miller@gmail.com>
2019-01-29 17:09:01 -0600

    welcome new user autopost

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,12 @@ const createNotification = notification => {
 		.catch(err => console.log('notification creation failed', err))
 }
 
+const createWelcomePost = post => {
+	return admin.firestore().collection('posts').add(post)
+		.then(doc => console.log('welcome post added', doc))
+		.catch(err => console.log('welcome post failed', err))
+}
+
 exports.postCreated = functions.firestore
 	.document('posts/{postId}')
 	.onCreate(doc => {
@@ -39,3 +45,24 @@ exports.userJoined = functions.auth.user()
 				return createNotification(notification)
 			})
 })
+
+exports.welcomePost = functions.auth.user()
+	.onCreate(user => {
+
+		return admin.firestore().collection('users')
+			.doc(user.uid).get().then(doc => {
+
+				const newUser = doc.data()
+				const post  = {
+					authorId: 'PostIt!',
+					authorFirstName: 'Post',
+					authorLastName: 'It!',
+					createdAt: admin.firestore.FieldValue.serverTimestamp(),
+					message: "Welcome to PostIt! Let's share some thoughts and stuff...",
+					title: `Hi, ${newUser.firstName} ${newUser.lastName}!`
+				}
+
+				return createWelcomePost(post)
+			})
+
+	})
